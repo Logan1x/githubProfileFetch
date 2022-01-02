f7e63e015ee1e97783571e218d8c9bbdefc8722a
logan1x <sharmakhushal78@gmail.com>
2022-01-02 17:39:47 +0530

    fix: fix for last commit, added a tag before so hyperlink can render

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           <p className="w-80 mx-auto text-center">Bio : {userData.bio}</p>
           <p>Location : {userData.location}</p>
           <p>
-            Website: <a href={userData.blog}></a> {userData.blog}
+            Website: <a href={userData.blog}> {userData.blog}</a>
           </p>
           <p className="text-lg text-red-600">{userData.message}</p>
         </div>
