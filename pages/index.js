4b30263918d49471ef186554615940d3cbc33adc
logan1x <sharmakhushal78@gmail.com>
2022-01-02 13:40:51 +0530

    Created github user finder app v1

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,71 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState("logan1x");
+  const [userData, setUserData] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const url = "https://api.github.com/users/";
+
+  // get user data from github api
+  const getUserData = async (user) => {
+    try {
+      const response = await fetch(url + user);
+      const data = await response.json();
+      setUserData(data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setIsLoading(true);
+      getUserData(user);
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  console.log(userData);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1 className="text-2xl font-semibold my-4">Get Your Github Details</h1>
+      <label htmlFor="inputUsername">Enter Username Below</label>
+      <input
+        className="border-2 border-black mb-4 text-center py-2 px-1 rounded"
+        value={user}
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+        placeholder="Enter username"
+        type="text"
+        id="inputUsername"
+      />
+      {error}
+      {userData ? (
+        <div className="flex flex-col items-center justify-center border-2 border-black rounded shadow-lg p-2 mx-2 space-y-2">
+          <img
+            className="h-60 w-60 rounded-full shadow-lg"
+            src={userData.avatar_url}
+            alt="avatar"
+          />
+          <h1>Name : {userData.name}</h1>
+          <p className="w-80 mx-auto text-center">Bio : {userData.bio}</p>
+          <p>Location : {userData.location}</p>
+          <p>Website: {userData.blog}</p>
+          <p>{userData.message}</p>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      ) : null}
+      <footer className="text-center text-gray-500 text-xs mt-4 ">
+        Created by{" "}
+        <span className="underline">
+          <a href="https://logan1x.github.io">Logan1x</a>
+        </span>
       </footer>
     </div>
-  )
+  );
 }
